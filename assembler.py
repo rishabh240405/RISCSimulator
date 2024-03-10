70d96f8886e6dbfc25b865db05e268dc55bd171f
shaurya23500 <162735608+shaurya23500@users.noreply.github.com>
2024-03-11 05:25:52 +0530

    Added branching

--- assembler.py
+++ assembler.py
@@ -1,10 +1,11 @@
 import re
-def dec_to_binary(imm):
+
+def dec_to_twos_comp(imm):
     n=int(imm)
     if n<0:
         abs_decimal=abs(n)
         bin_str ='0'+bin(abs_decimal)[2:]#[2:] because the first 2 letters are 0b denoting binary number,'0' is added to prevent overflow when 1 is added after inversion
-        inverted_bin_str = ''.join(['1' if bit == '0' else '0' for bit in bin_str])#bit inversion to generate 1's complement
+        inverted_bin_str = ''.join(['1' if bit=='0' else '0' for bit in bin_str])#bit inversion to generate 1's complement
         twos_complement=bin(int(inverted_bin_str, base=2)+1)[2:]#Adding 1 to the 1's complement to get the 2's complement(adding 1 in binary is same as adding 1 in decimal) because 1*2**0=1
         return str(twos_complement)
     else:
@@ -17,7 +18,7 @@ def hex_to_binary(n):
  
 def sext(imm): #Each register is 32 bit wide and thus each constant value(binary form) is extended to 32 bits
     if imm[0:2]=='0x': imm=hex_to_binary(imm[2:])
-    else: imm=dec_to_binary(imm)
+    else: imm=dec_to_twos_comp(imm)
     if len(imm)>32: return 'Error'
     signed_bit=imm[0]
     k=(32-len(imm))*signed_bit
@@ -54,16 +55,20 @@ def S_conversion(l_parts,instruction):#[sw,rs2,imm,rs1]
     line_encoded=imm[32-11-1:32-5]+dict_registers[rs2]+dict_registers[rs1]+dict_funct3_opcode[instruction][0]+imm[32-4-1:]+dict_funct3_opcode[instruction][1] 
     return line_encoded+'\n'
 
-def B_conversion(l_parts,instruction):#[instruction,rs1,rs2,imm/label]
+def B_conversion(l_parts,instruction,line_num):#[instruction,rs1,rs2,imm/label]
     dict_funct3_opcode={'beq':['000','1100011'],'bne':['001','1100011'],'blt':['100','1100011'],'bge':['101','1100011'],'bltu':['110','1100011'],'bgeu':['111','1100011']}
-    if(l_parts[3].isalnum()):
+    if(l_parts[3].isdigit() or (l_parts[3][0]=='-' and l_parts[3][1:].isdigit())):
         rs1=l_parts[1]
         rs2=l_parts[2]
         imm=sext(l_parts[3])
         line_encoded=(imm[32-12-1]+imm[32-10-1:32-5])+dict_registers[rs2]+dict_registers[rs1]+dict_funct3_opcode[instruction][0]+(imm[32-4-1:32-1]+imm[32-11-1])+dict_funct3_opcode[instruction][1] #imm[12|10:5]+rs2+rs1+funct3+imm[4:1|11]+opcode
         return line_encoded+'\n'
-    # else:
-    #     return
+    else:
+        rs1=l_parts[1]
+        rs2=l_parts[2]
+        imm=sext(str((line_num-dict_label[l_parts[3]])*4))
+        line_encoded=(imm[32-12-1]+imm[32-10-1:32-5])+dict_registers[rs2]+dict_registers[rs1]+dict_funct3_opcode[instruction][0]+(imm[32-4-1:32-1]+imm[32-11-1])+dict_funct3_opcode[instruction][1] #imm[12|10:5]+rs2+rs1+funct3+imm[4:1|11]+opcode
+        return line_encoded+'\n'
 
 def U_conversion(l_parts,instruction):#[instruction,rd,imm]
     dict_opcode={'lui':'0110111','auipc':'0010111'}
@@ -72,15 +77,18 @@ def U_conversion(l_parts,instruction):#[instruction,rd,imm]
     l_encoded=imm[32-31-1:32-12]+dict_registers[rd]+dict_opcode[instruction]
     return l_encoded+'\n'
 
-def J_conversion(l_parts,instruction):#[instruction,rd,imm]
+def J_conversion(l_parts,instruction,line_num):#[instruction,rd,imm]
     dict_opcode={'jal':'1101111'}
-    if(l_parts[2][1:].isalnum()): #[1:] because there can be a - sign in start
+    if(l_parts[2].isdigit() or (l_parts[2][0]=='-' and l_parts[2][1:].isdigit())): #[1:] because there can be a - sign in start
         rd=l_parts[1]
         imm=sext(l_parts[2])
         l_encoded=(imm[32-20-1]+imm[32-10-1:32-1]+imm[32-11-1]+imm[32-19-1:32-12])+dict_registers[rd]+dict_opcode[instruction]
         return l_encoded+'\n'
-    # else:
-    #     return
+    else:
+        rd=l_parts[1]
+        imm=sext(str((line_num-dict_label[l_parts[2]])*4))
+        l_encoded=(imm[32-20-1]+imm[32-10-1:32-1]+imm[32-11-1]+imm[32-19-1:32-12])+dict_registers[rd]+dict_opcode[instruction]
+        return l_encoded+'\n'
 
 # def bonus_conversion(line,instruction):
 #     return
@@ -88,6 +96,7 @@ def J_conversion(l_parts,instruction):#[instruction,rd,imm]
 with open('assembly.txt','r') as f:
     l_code_lines=[i.rstrip('\n') for i in f.readlines()]
 
+l_instructions=['add','sub','slt','sltu','xor','sll','srl','or','and','lw','addi','sltiu','jalr','sw','beq','bne','bge','bgeu','blt','bltu','auipc','lui','jal','mul','rst','halt','rvrs']
 l_base_instructions_R=['add','sub','slt','sltu','xor','sll','srl','or','and']
 l_base_instructions_I=['lw','addi','sltiu','jalr']
 l_base_instructions_S=['sw']
@@ -96,17 +105,24 @@ l_base_instructions_U=['auipc','lui']
 l_base_instructions_J=['jal']
 l_base_instructions_bonus=['mul','rst','halt','rvrs']
 dict_registers = {"zero": "00000","ra": "00001","sp": "00010","gp": "00011","tp": "00100","t0": "00101","t1": "00110","t2": "00111","s0": "01000","fp": "01000","s1": "01001","a0": "01010","a1": "01011","a2": "01100","a3": "01101","a4": "01110","a5": "01111","a6": "10000","a7": "10001","s2": "10010","s3": "10011","s4": "10100","s5": "10101","s6": "10110","s7": "10111","s8": "11000","s9": "11001","s10": "11010","s11": "11011","t3": "11100","t4": "11101","t5": "11110","t6": "11111"}
-dict_registers_values={}
-for i in dict_registers: dict_registers_values[i]='0'*32 
+
 l_machine_code=[]
-for i in l_code_lines:
-    l_parts=re.split(r'[,()\s]+',i)
+dict_label={}
+
+for i in range(len(l_code_lines)):
+    l_first=l_code_lines[i].lstrip(' ').split(' ')[0]
+    if l_first[len(l_first)-1]==':': dict_label[l_first.replace(':','')]=i+1
+
+for i in range(len(l_code_lines)):
+    l_parts=re.split(r'[,()\s]+',l_code_lines[i])
+    while(l_parts[0] not in l_instructions or len(l_parts)==0): l_parts.pop(0)
+    if len(l_parts)==0: continue
     if l_parts[0] in l_base_instructions_R: l_machine_code.append(R_conversion(l_parts,l_parts[0]))
     elif l_parts[0] in l_base_instructions_I: l_machine_code.append(I_conversion(l_parts,l_parts[0]))
     elif l_parts[0] in l_base_instructions_S: l_machine_code.append(S_conversion(l_parts,l_parts[0]))
-    elif l_parts[0] in l_base_instructions_B: l_machine_code.append(B_conversion(l_parts,l_parts[0]))
+    elif l_parts[0] in l_base_instructions_B: l_machine_code.append(B_conversion(l_parts,l_parts[0],i+1))
     elif l_parts[0] in l_base_instructions_U: l_machine_code.append(U_conversion(l_parts,l_parts[0]))
-    elif l_parts[0] in l_base_instructions_J: l_machine_code.append(J_conversion(l_parts,l_parts[0]))
+    elif l_parts[0] in l_base_instructions_J: l_machine_code.append(J_conversion(l_parts,l_parts[0],i+1))
     # elif l_parts[0] in l_base_instructions_bonus: str_machine_code+=bonus_conversion(i,l_parts[0])
     else: continue
     
